008b9c682685f020969cd9eae77769dbb25b0cf2
André Cordeiro <ahdncordeiro@gmail.com>
2020-03-25 16:43:10 -0300

    File creation

---
+++ google-hangouts-meet-keyboard-shortcut.js
@@ -0,0 +1,17 @@
+var shouldAddEventListener;
+
+// define a handler
+function doc_keyDown(e) {
+  // Test if keyCode 220 (\) was pressed
+  if (e.keyCode == 220) { // Test if keyCode 220 (\) was pressed
+    console.log("Keyboard to press More Options menu.");
+    document.querySelector("[data-tooltip='More options']").click()
+  }
+}
+
+// register the handler 
+if ((shouldAddEventListener == null ? true : shouldAddEventListener)) {
+  shouldAddEventListener = false;
+  console.log("Adding Google Hangouts Meet [Open More Options menu] keyboard shortcuts event listener...")
+  document.addEventListener('keydown', doc_keyDown, false);
+}
